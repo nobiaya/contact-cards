bcb6a5cd4208f296dcbb40907cd045bb877bda69
Shulammite-Aso <asoamarachi@gmail.com>
2020-05-29 13:42:36 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React World</h1>
+       <h2>
+             This is our first React App - isn't it marvellous?!
+       </h2>
+    </div>
+  );
+}
+
+export default App;
