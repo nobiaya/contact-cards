a6e2a349df079ff56b744c8ddc05ef5b832b7351
Shulammite-Aso <asoamarachi@gmail.com>
2020-06-10 11:19:29 +0000

    create contact list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React from "react";
+import "./styles.css";
 
 const App = () => {
+  const message = "This is my first variable rendered in JSX";
+  
   return (
-    <div>
-      <h1>Hello React World</h1>
-       <h2>
-             This is our first React App - isn't it marvellous?!
-       </h2>
+    <>
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jenny Han"
+        email="jenny.han@notreal.com"
+        age={25}
+      />
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jason Long"
+        email="jason.long@notreal.com"
+        age={45}
+      />
+      <ContactCard 
+        avatar="https://via.placeholder.com/150"
+        name="Peter Pan"
+        email="peter.pan@neverland.com"
+        age={100}
+      />
+    </>
+  );
+};
+
+const ContactCard = props => {
+  const [showAge, setShowAge] = React.useState(false);
+
+  return (
+    <div className="contact-card">
+      <img src={props.avatar} alt="profile" />
+      <div className="user-details">
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email} </p>
+        {/* line below is the same as {showAge === true ? <p>Age: 25</p> : null}*/}
+        {showAge && <p>Age: {props.age} </p> }
+        <button onClick={() => setShowAge(!showAge)}>
+          Toggle age
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
