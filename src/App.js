2e50767dc47cfad408ca0b116d2a3039c5206a38
Shulammite-Aso <asoamarachi@gmail.com>
2020-06-13 13:41:36 +0000

    render components from a list

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@ import React from "react";
 import "./styles.css";
 
 const App = () => {
-  const message = "This is my first variable rendered in JSX";
+
+  const contacts = [
+    { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
+    { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
+    { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 },
+    { name: "Amy McDonald", email: "amy@email.com", age: 33 }
+   ];
   
   return (
     <>
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jenny Han"
-        email="jenny.han@notreal.com"
-        age={25}
-      />
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jason Long"
-        email="jason.long@notreal.com"
-        age={45}
-      />
-      <ContactCard 
-        avatar="https://via.placeholder.com/150"
-        name="Peter Pan"
-        email="peter.pan@neverland.com"
-        age={100}
-      />
+      {contacts.map(contact => (
+        <ContactCard 
+         avatar="https://via.placeholder.com/150"
+         name={contact.name}
+         email={contact.email}
+         age={contact.age}
+        />
+      ))}
     </>
   );
 };
