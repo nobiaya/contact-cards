0a77bc08b22388ffd9ef15aca6599a7d3d283410
nobiaya <lilypromise99@gmail.com>
2021-01-20 07:00:13 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const ContactCard = props => {
       <div className="user-details">
         <p>Name: {props.name}</p>
         <p>Email: {props.email} </p>
-        <p>phone number: "08144566778" </p>
+        <p>name: "lily" </p>
         {/* line below is the same as {showAge === true ? <p>Age: 25</p> : null}*/}
         {showAge && <p>Age: {props.age} </p> }
         <p>text</p>
